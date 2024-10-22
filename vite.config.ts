1e702c70e55695043abdf3fb814202d50b6238f2
CRaLFa <minami.smd@gmail.com>
2024-10-22 12:26:12 +0900

    create template

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { crx, defineManifest } from '@crxjs/vite-plugin';
+import { defineConfig } from 'vite';
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: 'Stamp Downloader',
+  version: '0.0.1',
+  description: 'Download LINE stamp images',
+  icons: {},
+  action: {
+    default_icon: {},
+    default_popup: 'src/popup/index.html',
+  },
+  background: {
+    service_worker: 'src/background.ts',
+  },
+  content_scripts: [
+    {
+      js: ['src/content.ts'],
+      matches: [
+        'https://store.line.me/stickershop/product/*',
+      ],
+    },
+  ],
+  permissions: ['activeTab'],
+});
+
+export default defineConfig({
+  plugins: [
+    crx({ manifest }),
+  ],
+});
