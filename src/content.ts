ec2113c552d11907ec25877de6d1a165521331c9
CRaLFa <minami.smd@gmail.com>
2024-10-23 10:19:43 +0900

    rename

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,4 @@
-const reUrl = /url\("([^"]+)/;
+const RE_URL = /url\("([^"]+)/;
 
 const getImageUrls = () => {
   const spans = document.querySelectorAll<HTMLSpanElement>('ul.FnStickerList div.FnImage > span.mdCMN09Image');
@@ -6,7 +6,7 @@ const getImageUrls = () => {
     if (!span.attributeStyleMap.has('background-image')) {
       return null;
     }
-    const arr = reUrl.exec(span.attributeStyleMap.get('background-image')!.toString());
+    const arr = RE_URL.exec(span.attributeStyleMap.get('background-image')!.toString());
     return arr ? arr[1] : null;
   });
   return urls.filter((url) => url !== null);
